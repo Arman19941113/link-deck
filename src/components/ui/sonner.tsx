63d920b03a0e0ccae5b3d9a2c754eb02539ab91d
Arman Tang <td19941113@gmail.com>
2026-06-18 23:32:49 +0800

    fix: widen toast layout

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -28,6 +28,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
           "--normal-text": "var(--popover-foreground)",
           "--normal-border": "var(--border)",
           "--border-radius": "var(--radius)",
+          "--width": "32rem",
         } as CSSProperties
       }
       {...props}
